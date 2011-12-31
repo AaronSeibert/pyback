a800ee81f5ef61ef84bc9021a93973fefc5af184
Aaron Seibert <aaron@awseibert.net>
2011-12-31 11:37:11 -0500

    Added checking for backup type 'folder' to rackspace provider

--- backup_providers/rackspace.py
+++ backup_providers/rackspace.py
@@ -13,27 +13,36 @@ class Rackspace:
 		# API documentation states that best practice is to create a 0 or 1 byte object
 		# with content-type of application/directory so the virtual directories can 
 		# be enumerated using path=.  So that's what we do here
+		self.log += "Creating folder " + folder + "\n"
 		rsfolder = self.cont.create_object(folder)
 		rsfolder.content_type = "application/directory"
 		rsfolder.write("")
+		self.log += folder + " has been created.\n"
 	
-	def checkLocation(self, location):
+	def checkLocation(self, base_dir, backup_type):
+		self.base_dir = base_dir
+		self.backup_type = backup_type
+
 		try:	
 			# First we try to access the container.  If it exists, we set make the object
 			# available to the class
-			self.cont = self.conn.get_container(location)
+			self.cont = self.conn.get_container(base_dir)
 		except cloudfiles.errors.NoSuchContainer:
 			# If the container doesn't exist, we create it (and the subfolders)
-			self.log += location + " does not exist.  Creating...\n"
-			self.cont = self.conn.create_container(location)
-			self.log += location + " has been created.\n"
-			self.log += "Creating sub-containers...\n"
+			self.log += "Container " + self.base_dir + " does not exist.  Creating...\n"
+			self.cont = self.conn.create_container(self.base_dir)
+			self.log += "Container " + self.base_dir + " has been created.\n"
 
 			#Creates the subfolders
-			self.createSubFolder("Daily")
-			self.createSubFolder("Weekly")
-			self.createSubFolder("Monthly")
+			self.createSubFolder(self.backup_type)
 
-			self.log += "Sub-containers created.\n"
-		else:
-			self.log += "Using " + self.cont.name
+		self.log += "Using container " + self.cont.name + "\n"
+
+		try:
+			# Now we try to access the backup type "folder".
+			self.cont.get_object(self.backup_type)
+		except cloudfiles.errors.NoSuchObject:
+			# If the backup "folder" doesn't exist, create it
+			self.log += "Folder " + self.backup_type + " does nto exist.\n"
+			self.createSubFolder(self.backup_type)
+			
