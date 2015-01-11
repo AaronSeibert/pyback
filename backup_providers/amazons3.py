d5de84f64c6abdfdc7a3a6ed2543da114cfc1ce6
Aaron Seibert <aaron@binaryitsystems.com>
2015-01-11 01:57:43 -0500

    no dill for your pickle

--- backup_providers/amazons3.py
+++ backup_providers/amazons3.py
@@ -6,12 +6,42 @@ import glob
 import subprocess
 import contextlib
 import functools
-#import multiprocessing
-import dill
-from pathos.multiprocessing import ProcessingPool
+import multiprocessing
 import boto
 import traceback
 from multiprocessing.pool import IMapIterator
+
+def _pickle_method(method):
+    """
+    Author: Steven Bethard (author of argparse)
+    http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
+    """
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    cls_name = ''
+    if func_name.startswith('__') and not func_name.endswith('__'):
+        cls_name = cls.__name__.lstrip('_')
+    if cls_name:
+        func_name = '_' + cls_name + func_name
+    return _unpickle_method, (func_name, obj, cls)
+
+
+def _unpickle_method(func_name, obj, cls):
+    """
+    Author: Steven Bethard
+    http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
+    """
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    return func.__get__(obj, cls)
+
+copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
 
 class AmazonS3:
 	def __init__(self, keyid, key):
